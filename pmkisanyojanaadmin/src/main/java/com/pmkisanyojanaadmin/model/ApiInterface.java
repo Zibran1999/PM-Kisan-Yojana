d6f862ad37780c67a7281ea17c6a4584580f1682
Cypher103360 <gk3854631@gmail.com>
2021-11-08 14:29:39 +0530

    Modified dialogs

--- pmkisanyojanaadmin/src/main/java/com/pmkisanyojanaadmin/model/ApiInterface.java
+++ pmkisanyojanaadmin/src/main/java/com/pmkisanyojanaadmin/model/ApiInterface.java
@@ -1,10 +1,24 @@
 package com.pmkisanyojanaadmin.model;
 
+import java.util.Map;
+
 import retrofit2.Call;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface ApiInterface {
 
     @POST("fetch_kisan_yojana_api.php")
     Call<YojanaModelList> getAllYojana();
+
+    @FormUrlEncoded
+    @POST("upload_kisan_yojna_api.php")
+    Call<MessageModel> uploadYojna(@FieldMap Map<String,String> map);
+
+    @FormUrlEncoded
+    @POST("upload_news_api.php")
+    Call<MessageModel> uploadNews(@FieldMap Map<String,String> map);
+
+
 }
